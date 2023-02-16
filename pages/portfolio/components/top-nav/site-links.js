8a2135d0d8cc49b83b461f796d725b700303e9f2
moise <moisemlg90@gmail.com>
2023-02-17 00:22:50 +0200

    Create a site links component

---
+++ pages/portfolio/components/top-nav/site-links.js
@@ -0,0 +1,10 @@
+import next from "next";
+
+export default function SiteLinks(props) {
+    const { data } = props;
+    return (
+        <div>
+            <h1>My portfolio</h1>
+        </div>
+    );
+}
