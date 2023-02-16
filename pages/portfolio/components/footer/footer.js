b10809662d6925ce022ecbed7f1694fa796081f8
moise <moisemlg90@gmail.com>
2023-02-17 00:23:37 +0200

    Create a footer component

---
+++ pages/portfolio/components/footer/footer.js
@@ -0,0 +1,10 @@
+import next from "next";
+
+export default function Footer(props) {
+    const { data } = props;
+    return (
+        <div>
+            <h1>My portfolio</h1>
+        </div>
+    );
+}
