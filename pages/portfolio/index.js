50434b53dadff46fa74ddad0d30703f956ee34ab
moise <moisemlg90@gmail.com>
2023-02-17 00:20:00 +0200

    Create a portfolio component

---
+++ pages/portfolio/index.js
@@ -0,0 +1,11 @@
+import next from "next";
+
+export default function Portfolio(props) {
+    const { data } = props;
+
+    return (
+        <div>
+        <h1>My portfolio</h1>
+        </div>
+    );
+}
