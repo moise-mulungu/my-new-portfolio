fee22e012ec16f949dad9a24c7f6e07f53d82038
moise <moisemlg90@gmail.com>
2023-02-18 20:32:11 +0200

    Import/export functions

--- pages/portfolio/components/content/contents.js
+++ pages/portfolio/components/content/contents.js
@@ -1,10 +1,10 @@
 import next from "next";
 
-export default function Content (props) {
-    const { data } = props;
-    return (
-        <div>
-            <h1>My portfolio</h1>
-        </div>
-    );
+export function Content(props) {
+  const { data } = props;
+  return (
+    <div>
+      <h1>My portfolio</h1>
+    </div>
+  );
 }
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,16 +1,16 @@
 // import Head from "next/head";
-import { SiteLinks } from './components/top-nav/site-links'
-import { Footer } from './components/footer/footer'
-import { Content } from './components/content/contents'
+import { SiteLinks } from "./components/top-nav/site-links";
+import { Footer } from "./components/footer/footer";
+import { Content } from "./components/content/contents";
 
 export default function Portfolio(props) {
-    const { data } = props;
+  const { data } = props;
 
-    return (
-        <>
-            <SiteLinks />
-            <Footer />
-            <Content />
-        </>
-    );
+  return (
+    <>
+      <SiteLinks />
+      <Footer />
+      <Content />
+    </>
+  );
 }
